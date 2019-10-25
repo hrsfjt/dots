680e8a0206459410402557cbc7f7e983dfd289b1
hf-mush <fuchantoshu@gmail.com>
2019-10-26 00:26:48 +0900

    add: .env for local path

--- git/gitconfig.sh
+++ git/gitconfig.sh
@@ -1,12 +1,14 @@
 #!/bin/zsh
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+. $SCRIPT_DIR/.env
 
-git config --global user.name ""
-git config --global user.email ""
+git config --global user.name $USER_NAME
+git config --global user.email $USER_EMAIL
 
 git config --global core.ignorecase false
 git config --global core.quotepath false
 git config --global core.editor 'vim -c "set fenc=utf-8"'
-git config --global core.excludesfile ""
+git config --global core.excludesfile $GITIGNORE_PATH
 
 git config --global color.diff auto
 git config --global color.status auto
